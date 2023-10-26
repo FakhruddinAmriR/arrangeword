71f0112023adae58eb4bb1d4f77fa0a11dbf6c6c
Fakhruddin Amri Rakhmatullah <105553920+FakhruddinAmriR@users.noreply.github.com>
2023-10-26 11:05:26 +0700

    First Commit

---
+++ Assets/Scripts/LetterScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Sprites;
+
+public class LetterScript : MonoBehaviour
+{
+    public void Attack()
+    {
+        
+    }
+}
